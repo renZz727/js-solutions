2409b34430043be5289d104ead8c43ebf56855e2
rensonkroji02@gmail.com <rensonkroji02@gmail.com>
2025-10-29 20:27:15 +0530

    commit

--- test.js
+++
@@ -1,6 +0,0 @@
-// function getIds() {
-//     1. collect all the <p> using the "id" (list = document.querySelectorAll(".list_item"))
-//     2. loop through the list 
-//     3. split each item using split('-') opertor and store in a array (usersDetails)
-//     4. display all the usersDetails[1];
-// }
